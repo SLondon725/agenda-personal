ace3f16d1293bb71696be697cee1c2b11961f02c
SLONDON725 <decriloba204@gmail.com>
2025-04-16 15:59:08 -0500

    Trabajando el script para el index y creando archivo tareas.html asi como su JS

--- js/script.js
+++ js/script.js
@@ -1,14 +1,39 @@
-document.getElementById('todayDate').textContent = new Date().toLocaleDateString(); // Sacar fecha local;
+document.getElementById('todayDate').textContent = new Date().toLocaleDateString(); // Sacar fecha local
+const hoy = new Date().toISOString().split('T')[0]; // Sacar la fecha local en formato ingles
+const modal = document.getElementById('modalFormulario'); // Modal del nuevo registro
+const btnGuardar = document.getElementById('guardar');  // Guardar nuevo registro
 
-// Funcion para guardar datos del modal
-function guardarDatos() {
-    const titulo = document.getElementById("titulo").value;
-    const descripcion = document.getElementById("descripcion").value;
-    
+// evento para reiniciar el modal 
+modal.addEventListener('hidden.bs.modal',()=> { document.getElementById('formModal').reset();  });
+
+btnGuardar.addEventListener('click',()=>{
+  // Trayendo los valores del nuevo registro
+  const titulo = document.getElementById("titulo").value;
+  const descripcion = document.getElementById("descripcion").value;
+  const fecha = document.getElementById("fecha").value;
+  const prioridad = document.getElementById("prioridad").value;
+
+  // Validando si faltan datos
+  if (titulo === "" || descripcion === "" || fecha === "") {
+    Swal.fire({
+      title: 'Campos Incompletos',
+      icon: 'warning',
+      text: 'Debe llenar todos los campos',
+      position: 'top',
+      toast: true,         // Hace que sea un mensajito como "toast"
+      timer: 3000,         // Se cierra en 3 segundos
+      showConfirmButton: false
+    });
+  }else{
     console.log("Título:", titulo);
     console.log("Descripción:", descripcion);
-
-    // Cerrar el modal manualmente
-    const modal = bootstrap.Modal.getInstance(document.getElementById('modalFormulario'));
-    modal.hide();
+    console.log("Fecha:", fecha);
+    console.log("Prioridad:", prioridad);
+    Swal.fire({
+      title: '¡Nuevo registro!',
+      text: 'Tu tarea se registro de forma correcta',
+      icon: 'success',
+      confirmButtonText: 'OK'
+    });
   }
+})
